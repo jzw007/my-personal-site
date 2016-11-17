146edca6b8ed87adc29775e95384a64dc9383914
jeremiahwood <jwood47@cnm.edu>
2016-11-17 13:34:25 -0700

    bootstap index

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf 8">
+		<meta http-equiv="x-ua-compatible"content="ie=edge">
+		<meta name="viewport" content="width=device-width, initial scale=1">
+
+		<!--latest compiled and minified css-->
+		<!-- Bootstrap Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
+
+		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+
+		<!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+		<link type="text/css" href="../css/style.css" rel="stylesheet" />
+		<title>Boo</title>
+	</head>
+
+	<!--content starts here-->
+	<body>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6"> Column One: 6/12 columns, 50% of container width. </div>
+				<div class="col-md-6"> Column Two: 6/12 columns, 50% of container width. </div>
+			</div>
+
+			<div class="row">
+				<h1>BoYah BootStrap</h1>
+				<div class="col-md-4"> Column Three: 4/12 columns, 33.33% of container width. </div>
+				<div class="col-md-4"> Column Four: 4/12 columns, 33.33% of container width. </div>
+				<div class="col-md-4"> Column Five: 4/12 columns, 33.33% of container width. </div>
+			</div>
+		</div>
+	</body>
+</html>
