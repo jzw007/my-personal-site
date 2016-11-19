d7d1d3056be8c51782b4303d4417b22744020880
jeremiahwood <jwood47@cnm.edu>
2016-11-19 12:01:15 -0700

    Updated Header, and nav bar

--- index.php
+++ index.php
@@ -29,47 +29,27 @@
 	<!--content starts here-->
 	<body>
 		<div class="container">
-			<div class="row">
-				<div class="col-md-6"> Column One: 6/12 columns, 50% of container width. </div>
-				<div class="col-md-6"> Column Two: 6/12 columns, 50% of container width. </div>
-			</div>
 
 			<div class="row">
-				<h1>BoYah BootStrap</h1>
-				<div class="col-md-4"> Column Three: 4/12 columns, 33.33% of container width. </div>
-				<div class="col-md-4"> Column Four: 4/12 columns, 33.33% of container width. </div>
-				<div class="col-md-4"> Column Five: 4/12 columns, 33.33% of container width. </div>
+				<div class="col-xs-12">
+					<div class="jumbotron">
+						<h1>Jeremiah Z. Wood</h1>
+						<p>Albuquerque, N.M.</p>
+					</div>
+				</div>
 			</div>
+
+
+			<div class="row">
+				<div class="col-xs-12">
+
+
+				</div>
+			</div>
+
+
+
 		</div>
 	</body>
 </html>
-
-<html>
-
-	<!-- thanks bootstrap: http://getbootstrap.com/components/#navbar -->
-	<nav class="navbar navbar-default">
-		<div class="container-fluid">
-			<!-- Brand and toggle get grouped for better mobile display -->
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-					<span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="#">Brand</a>
-			</div>
-
-			<!-- Collect the nav links, forms, and other content for toggling -->
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				<ul class="nav navbar-nav navbar-right">
-					<li><a href="<?php echo $PREFIX; ?>index.php">Home</a></li>
-					<li><a href="<?php echo $PREFIX; ?>astronomy.php">Astronomy Apps</a></li>
-					<li><a href="<?php echo $PREFIX; ?>demos.php">Games and Demos</a></li>
-					<li><a href="<?php echo $PREFIX; ?>java.php">Java Downloads</a></li>
-					<li><a href="<?php echo $PREFIX; ?>resume.pdf">Resume</a></li>
-				</ul>
-			</div><!-- /.navbar-collapse -->
-		</div><!-- /.container-fluid -->
-	</nav>
 
