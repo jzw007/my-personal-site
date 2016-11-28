b011b91f1cb33705a6b505498a1056582f89c9a3
jeremiahwood <jwood47@cnm.edu>
2016-11-28 10:05:34 -0700

    Search box

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 		<meta charset="utf 8">
 		<meta http-equiv="x-ua-compatible" content="ie=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
-<!--Fonts for the page-->
+		<!--Fonts for the page-->
 		<link href="https://fonts.googleapis.com/css?family=Dancing+Script" rel="stylesheet">
 
 
@@ -25,14 +25,26 @@
 		<!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
+		<nav class="navbar navbar-default navbar-fixed-top">
+			<div class="container">
+				...
+			</div>
+		</nav>
+
+<!--	insert for the flickity carousel-->
 		<link rel="stylesheet" href="css/flickity.css">
 		<script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
 
 		<link type="text/css" href="css/style.css" rel="stylesheet" />
-		<title>Boo</title>
+		<title>Jeremiah Z. Wood</title>
+
+		<!--Insert Nice and Simple contact Form-->
+		<link rel="stylesheet" href="NiceSimpleContactForm/style.css">
+		<link rel="stylesheet" href="NiceSimpleContactForm/index.html">
+		<script src="https://css-tricks.com/nice-and-simple-contact-form"></script>
 	</head>
 
-	<!--content starts here-->
+	<!--content of main page starts here-->
 	<body>
 		<div class="container">
 			<div class="row">
@@ -44,58 +56,64 @@
 				</div>
 			</div>
 			<div class="container">
-				<div class="col-xs-12">
-					<nav>
-						<ul>
-							<li><a href="Resume.htm"></a>Resume</li>
-							<li><a href="Bio.htm"></a>Bio</li>
-							<li><a href="Contact"></a>Contact</li>
-						</ul>
-					</nav>
+				<div class="row">
+					<div class="col-xs-12">
+						<nav> class="navbar navbar-default navbar-fixed-top">
+								<div class="container">
+								</div>
+							</nav>
+					</div>
 				</div>
 			</div>
 		</div>
 
 
 
-			<div class="row">
-				<div class="col-md-6">
-					<h1 class="column-label">
-						<strong>Headshots</strong>
-					</h1>
+		<div class="row">
+			<div class="col-md-6">
+				<h1 class="column-label">
+					<strong>Headshots</strong>
+				</h1>
 
-					<div class="main-carousel" data-flickity>
-						<div class="carousel-cell">
-<!--							<img class="img-responsive" src="media/Headh shot 004-07-12-2013.png">-->
-<!--							<a href="media/Headh shot 004-07-12-2013.png">Download</a>-->
-						</div>
-
-						<div class="carousel-cell">
-							<img class="img-responsive" src="media/head shot 001 on 11-15-16.png">
-							<a href="media/head shot 001 on 11-15-16.png">Download</a>
-						</div>
-
-						<div class="carousel-cell">
-							<img class="img-responsive" src="media/head shot 002, 11-15-16.png">
-							<a href="media/head shot 002, 11-15-16.png">Download</a>
-						</div>
-
-						<div class="carousel-cell">
-							<img class="img-responsive" src="media/Headshot 00311-15-16.png">
-							<a href="media/Headshot 00311-15-16.png">Download</a>
-						</div>
-
+				<div class="main-carousel" data-flickity>
+					<div class="carousel-cell">
+						<!--<img class="img-responsive" src="media/Headh shot 004-07-12-2013.png">-->
+						<!--<a href="media/Headh shot 004-07-12-2013.png">Download</a>-->
 					</div>
-				</div>
-				<div class="col-md-6">
-					<h1 class="column-label">
-						<strong>Video Real</strong>
-					</h1>
-					<p class="embed-responsive embed-responsive-16by9">
-						<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/PvkcqgpZJCw" frameborder="0" allowfullscreen></iframe>
-					</p>
+
+					<div class="carousel-cell">
+						<img class="img-responsive" src="media/head shot 001 on 11-15-16.png">
+						<a href="media/head shot 001 on 11-15-16.png">Download</a>
+					</div>
+
+					<div class="carousel-cell">
+						<img class="img-responsive" src="media/head shot 002, 11-15-16.png">
+						<a href="media/head shot 002, 11-15-16.png">Download</a>
+					</div>
+
+					<div class="carousel-cell">
+						<img class="img-responsive" src="media/Headshot 00311-15-16.png">
+						<a href="media/Headshot 00311-15-16.png">Download</a>
+					</div>
+
 				</div>
 			</div>
+			<div class="col-md-6">
+				<h1 class="column-label">
+					<strong>Video Real</strong>
+				</h1>
+				<p class="embed-responsive embed-responsive-16by9">
+					<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/PvkcqgpZJCw" frameborder="0" allowfullscreen></iframe>
+
+					<div class="col-md-6">
+					<div class="row">
+						<div class="col-xs-12">
+							<div class="NiceSimpleContactForm">
+
+							</div>
+				</p>
+			</div>
+		</div>
 		</div>
 	</body>
 </html>
