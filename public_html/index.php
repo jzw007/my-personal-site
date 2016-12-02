31e60b50273cfdc92b8af333aca2bb0650e0b8fe
jeremiahwood <jwood47@cnm.edu>
2016-12-01 19:24:56 -0700

    Started to template

--- public_html/index.php
+++ public_html/index.php
@@ -1,59 +1,13 @@
-<!DOCTYPE html>
-<html lang="en" xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html">
-	<head>
-		<meta charset="utf 8">
-		<meta http-equiv="x-ua-compatible" content="ie=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1"/>
-		<!--Fonts for the page-->
-		<link href="https://fonts.googleapis.com/css?family=Dancing+Script" rel="stylesheet">
+<?php
+/*grab current directory*/
+$CURRENT_DIR = __DIR__;
 
+// set page title
+$PAGE_TITLE = "JZW";
 
-		<!--latest compiled and minified css-->
-		<!-- Bootstrap Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
-				crossorigin="anonymous"/>
-
-		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
-		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-		<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-		<![endif]-->
-
-		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
-
-		<!-- jQuery Form, Additional Methods, Validate -->
-		<script type="text/javascript"
-				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
-		<script type="text/javascript"
-				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/jquery.validate.min.js"></script>
-		<script type="text/javascript"
-				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/additional-methods.min.js"></script>
-
-
-		<!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
-				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
-				  crossorigin="anonymous"></script>
-
-
-		<!-- Your JavaScript Form Validator -->
-		<script src="js/jquery-validate.js"></script>
-
-		<!--	insert for the flickity carousel-->
-		<link rel="stylesheet" href="css/flickity.css">
-		<script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
-
-		<link type="text/css" href="css/style.css" rel="stylesheet"/>
-		<title>Jeremiah Z. Wood</title>
-
-		<!--insert reCAPTCHA Google-->
-		<script src='https://www.google.com/recaptcha/api.js'></script>
-
-	</head>
-
+//load head-utils.php
+require_once("php/partials/head-utils.php");
+?>
 	<!--content of main page starts here-->
 	<body>
 		<div class="container">
