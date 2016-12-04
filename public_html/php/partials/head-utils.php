d652b19ce269f6c8da89f2468049bbb072c482d0
jeremiahwood <jwood47@cnm.edu>
2016-12-03 17:39:28 -0700

    edited the header

--- public_html/php/partials/head-utils.php
+++ public_html/php/partials/head-utils.php
@@ -18,7 +18,7 @@ $PREFIX = str_repeat("../", $DEPTH_DIFFERENCE);
 		<meta http-equiv="x-ua-compatible" content="ie=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 		<!--Fonts for the page-->
-		<link href="https://fonts.googleapis.com/css?family=Dancing+Script" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Slackey" rel="stylesheet">
 
 
 		<!--latest compiled and minified css-->
